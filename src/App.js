154630e25fa61f30e9444cbca7a914597b2089e1
YonovDev <yonovdesign@gmail.com>
2019-08-15 17:50:22 -0700

    Dynamic Componet Generation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+function Food({name, picture}) {
+  return (
+    <div>
+      <h1>I Like {name}</h1>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image: "https://i.ytimg.com/vi/eTucCw1w6Ak/maxresdefault.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image: "https://swisshansik.files.wordpress.com/2011/10/samgyupsal.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image: "https://i.ytimg.com/vi/6QQ67F8y2b8/maxresdefault.jpg"
+  },
+  {
+    name: "Doncasu",
+    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image: "http://chefjulieyoon.com/wp-content/uploads/2011/12/Kimbop-4.jpg"
+  }
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>Hello!!!!!</h1>
+    {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
+  </div>
   );
 }
 
